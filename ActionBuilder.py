f7bdf0d445927ba4585e8b1b9c57db12855f3f87
Aaron Chambers <aaron.chambers81@gmail.com>
2019-04-06 23:26:33 -0400

    added mute button

--- ActionBuilder.py
+++ ActionBuilder.py
@@ -4,7 +4,7 @@ from Color import Color
 class ActionBuilder():
 
 	@staticmethod
-	buildKeys(params, actions, inputs, low, high, col1=Color.white(), col2=Color.white()):
+	def buildKeys(params, actions, inputs, low, high, col1=Color.white(), col2=Color.white()):
 		for i in range(low, high):
 			color = Color.interpolate(col1, col2, float((i-low)/(high-low)))
 			action = ActionColorTrigger(params, color)
---
+++ actions/ActionMute.py
@@ -0,0 +1,12 @@
+from actions.Action import Action
+
+class ActionMute(Action):
+	def __init__(self, params):
+		super(ActionMute, self).__init__(params)
+		self.settings["On"] = 0
+
+	def update(self, params):
+		if self.settings["On"] > 127:
+			self.settings["MUTE"] = True
+		else:
+			self.settings["MUTE"] = False
