19568fc9f3f0296943abe3e9c1185ca320aa8ba7
Aaron Chambers <aaron.chambers81@gmail.com>
2019-04-07 02:04:08 +0100

    fixed builder

--- ActionBuilder.py
+++ ActionBuilder.py
@@ -1,11 +1,13 @@
 from actions.ActionColorTrigger import ActionColorTrigger
+from midi_in.InputControl import InputControl
 from Color import Color
 
-class ActionBuilder():
+class ActionBuilder:
 
 	@staticmethod
-	buildKeys(params, actions, inputs, low, high, color=Color.white()):
+	def buildKeys(params, actions, inputs, low, high, color=Color.white()):
 		for i in range(low, high):
+                        print i
 			action = ActionColorTrigger(params, color)
 			actions.append(action)
-			inputs.append(InputControl(action, type, i, ""))
+			inputs.append(InputControl(action, "trigger", i, ""))
