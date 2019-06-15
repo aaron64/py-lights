0674cd435cf037f2b67fd6d06c6abffc20215fea
Aaron Chambers <aaron.chambers81@gmail.com>
2019-06-15 01:46:04 +0100

    fixed issue with color class string representation

--- Color.py
+++ Color.py
@@ -1,62 +1,67 @@
 import random
 
-class Color:
 
-    def __init__(self, r, g, b):
-    	self.r = min(r, 255)
-    	self.g = min(g, 255)
-    	self.b = min(b, 255)
+class Color():
 
-    @classmethod
-    def interpolate(cls, col1, col2, val):
-    	r = int((col2.r - col1.r) * val + col1.r)
-    	g = int((col2.g - col1.g) * val + col1.g)
-    	b = int((col2.b - col1.b) * val + col1.b)
-    	return cls(r, g, b)
-    
-    @classmethod
-    def white(cls):
-        return cls(255, 255, 255)
 
-    @classmethod
-    def black(cls):
-    	return cls(0, 0, 0)
+	def __init__(self, r, g, b):
+		self.r = min(r, 255)
+		self.g = min(g, 255)
+		self.b = min(b, 255)
 
-    @classmethod
-    def red(cls):
-        return cls(255, 0, 0)
+	def __unicode__(self):
+		return u"" + str(self.r) + "," + str(self.g) + "," + str(self.b)
 
-    @classmethod
-    def green(cls):
-        return cls(0, 255, 0)
+	@classmethod
+	def interpolate(cls, col1, col2, val):
+		r = int((col2.r - col1.r) * val + col1.r)
+		g = int((col2.g - col1.g) * val + col1.g)
+		b = int((col2.b - col1.b) * val + col1.b)
+		return cls(r, g, b)
+	
+	@classmethod
+	def white(cls):
+		return cls(255, 255, 255)
 
-    @classmethod
-    def blue(cls):
-        return cls(0, 0, 255)
+	@classmethod
+	def black(cls):
+		return cls(0, 0, 0)
 
-    @classmethod
-    def cyan(cls):
-        return cls(0, 255, 255)
+	@classmethod
+	def red(cls):
+		return cls(255, 0, 0)
 
-    @classmethod
-    def yellow(cls):
-        return cls(255, 255, 0)
+	@classmethod
+	def green(cls):
+		return cls(0, 255, 0)
 
-    @classmethod
-    def magenta(cls):
-        return cls(255, 0, 255)
+	@classmethod
+	def blue(cls):
+		return cls(0, 0, 255)
 
-    @classmethod
-    def getRandomColor(cls):
-    	i = random.randint(0,7)
-    	return {
-    		'0': cls.white(),
-    		'1': cls.black(),
-    		'2': cls.red(),
-    		'3': cls.green(),
-    		'4': cls.blue(),
-    		'5': cls.cyan(),
-    		'6': cls.yellow(),
-    		'7': cls.magenta()
-    	}[str(i)]
+	@classmethod
+	def cyan(cls):
+		return cls(0, 255, 255)
+
+	@classmethod
+	def yellow(cls):
+		return cls(255, 255, 0)
+
+	@classmethod
+	def magenta(cls):
+		return cls(255, 0, 255)
+
+	@classmethod
+	def getRandomColor(cls):
+		i = random.randint(0,7)
+		return {
+			'0': cls.white(),
+			'1': cls.black(),
+			'2': cls.red(),
+			'3': cls.green(),
+			'4': cls.blue(),
+			'5': cls.cyan(),
+			'6': cls.yellow(),
+			'7': cls.magenta()
+		}[str(i)]
 	
