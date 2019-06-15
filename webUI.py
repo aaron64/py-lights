cad8885e5d31dbeb10a12480cd346682df39f2b8
Aaron Chambers <aaron.chambers81@gmail.com>
2019-06-14 23:14:21 -0400

    testing tables

--- webUI.py
+++ webUI.py
@@ -5,10 +5,12 @@ import threading
 
 data = 'foo'
 webUI = Flask(__name__)
+data = {}
 
 @webUI.route("/")
 def main():
-	return render_template('index.html')
+	return render_template('index.html', actions=data.actions)
 
 def initialize_ui(app):
+	data = app
 	threading.Thread(target=webUI.run(host='0.0.0.0', port=5000)).start()
